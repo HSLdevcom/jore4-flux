4529c127f626063f4f0f768dcd64a0dbc3fce55e
Tamas Pallos <tamas.pallos@vincit.fi>
2021-05-21 11:15:28 +0300

    Setting up e2e cluster remotely

---
+++ remotecluster.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -eu
+
+echo "Creating temporary directory for cluster definition"
+tmp_dir=$(mktemp -d e2e-kustomize-XXXXXXXXXX)
+
+# Making sure that the temp directory is cleaned up even if the script fails
+cleanup() {
+  echo "Cleaning up"
+  rm -rf "$tmp_dir"
+}
+trap cleanup EXIT
+
+echo "Downloading helper script"
+curl https://raw.githubusercontent.com/HSLdevcom/jore4-flux/main/kindcluster.sh --output "$tmp_dir/kindcluster.sh"
+chmod +x "$tmp_dir/kindcluster.sh"
+
+# Loading the base e2e cluster definition, then patch it:
+# - FRONTEND_DOCKER_IMAGE env variable is defined, the frontend image in the Kind cluster will be
+# replaced with image in the value, e.g. FRONTEND_DOCKER_IMAGE="hsldevcom/jore4-ui:abc"
+# - BACKEND_DOCKER_IMAGE env variable is defined, the frontend image in the Kind cluster will be
+# replaced with image in the value, e.g. BACKEND_DOCKER_IMAGE="hsldevcom/jore4-backend:def"
+echo "Customizing cluster definition"
+cat <<EOT >"$tmp_dir/kustomization.yaml"
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+  # have to use the go-getter url format: https://github.com/hashicorp/go-getter#url-format
+  - github.com/HSLdevcom/jore4-flux/clusters/e2e?ref=remote-e2e-testing
+
+patchesStrategicMerge:
+- |-
+  apiVersion: apps/v1
+  kind: Deployment
+  metadata:
+    name: jore4-frontend
+    namespace: hsl-jore4
+  spec:
+    template:
+      spec:
+        containers:
+        - name: jore4-frontend-image
+          ${FRONTEND_DOCKER_IMAGE:+image: $FRONTEND_DOCKER_IMAGE}
+- |-
+  apiVersion: apps/v1
+  kind: Deployment
+  metadata:
+    name: jore4-backend
+    namespace: hsl-jore4
+  spec:
+    template:
+      spec:
+        containers:
+        - name: jore4-backend-image
+          ${BACKEND_DOCKER_IMAGE:+image: $BACKEND_DOCKER_IMAGE}
+EOT
+
+echo "Testing that cluster definition builds correctly"
+kustomize build "$tmp_dir"
+
+echo "Downloading Kind config"
+curl https://raw.githubusercontent.com/HSLdevcom/jore4-flux/main/kind-cluster.yaml --output "$tmp_dir/kind-cluster.yaml"
+
+echo "Starting Kind and deploying the cluster"
+"$tmp_dir/kindcluster.sh" start --kindconfig="$tmp_dir/kind-cluster.yaml" --cluster="$tmp_dir"
