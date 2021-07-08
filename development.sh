01acf4a82e1521c9d55271170188ba5fd41563b8
Tamas Pallos <tamas.pallos@vincit.fi>
2021-07-08 15:18:40 +0300

    WIP: e2e cluster builds

--- development.sh
+++ development.sh
@@ -3,17 +3,17 @@
 set -eu
 
 function generate_kustomize_patches {
-  echo "Generating Kustomize patches with gomplate"
+  echo "Generating Kubernetes manifests with gomplate"
 
-  GOMPLATE_CMD="docker run --rm -v $(pwd):/tmp hairyhenderson/gomplate@sha256:8e46d887a73ef5d90fde1f1a7d679fa94cf9f6dfc686b0b1a581858faffb1e16"
-  $GOMPLATE_CMD --input-dir /tmp/generate/templates/kustomize-patches --output-dir /tmp/clusters/playg --context Values=/tmp/generate/values/playg.yaml
-  $GOMPLATE_CMD --input-dir /tmp/generate/templates/kustomize-patches --output-dir /tmp/clusters/dev --context Values=/tmp/generate/values/dev.yaml
-  $GOMPLATE_CMD --input-dir /tmp/generate/templates/kustomize-patches --output-dir /tmp/clusters/test --context Values=/tmp/generate/values/test.yaml
-  $GOMPLATE_CMD --input-dir /tmp/generate/templates/kustomize-patches --output-dir /tmp/clusters/prod --context Values=/tmp/generate/values/prod.yaml
-  $GOMPLATE_CMD --template templates=/tmp/generate/templates/resources/ --input-dir /tmp/generate/templates/kubernetes --output-dir /tmp/clusters/playg -c "Values=merge:common|env" -d "common=/tmp/generate/values/common.yaml" -d "env=/tmp/generate/values/playg.yaml"
-  $GOMPLATE_CMD --template templates=/tmp/generate/templates/resources/ --input-dir /tmp/generate/templates/kubernetes --output-dir /tmp/clusters/dev -c "Values=merge:common|env" -d "common=/tmp/generate/values/common.yaml" -d "env=/tmp/generate/values/dev.yaml"
-  $GOMPLATE_CMD --template templates=/tmp/generate/templates/resources/ --input-dir /tmp/generate/templates/kubernetes --output-dir /tmp/clusters/test -c "Values=merge:common|env" -d "common=/tmp/generate/values/common.yaml" -d "env=/tmp/generate/values/test.yaml"
-  $GOMPLATE_CMD --template templates=/tmp/generate/templates/resources/ --input-dir /tmp/generate/templates/kubernetes --output-dir /tmp/clusters/prod -c "Values=merge:common|env" -d "common=/tmp/generate/values/common.yaml" -d "env=/tmp/generate/values/prod.yaml"
+  GOMPLATE_CMD="docker run --rm -v $(pwd):/tmp hairyhenderson/gomplate@sha256:8e46d887a73ef5d90fde1f1a7d679fa94cf9f6dfc686b0b1a581858faffb1e16 --template templates=/tmp/generate/templates/resources/ -c \"Values=merge:common|env\" -d \"common=/tmp/generate/values/common.yaml\""
+  $GOMPLATE_CMD --input-dir /tmp/generate/templates/kubernetes --output-dir /tmp/clusters/e2e -d "env=/tmp/generate/values/e2e.yaml"
+  $GOMPLATE_CMD --input-dir /tmp/generate/templates/kubernetes --output-dir /tmp/clusters/playg -d "env=/tmp/generate/values/playg.yaml"
+  $GOMPLATE_CMD --input-dir /tmp/generate/templates/kubernetes --output-dir /tmp/clusters/dev -d "env=/tmp/generate/values/dev.yaml"
+  $GOMPLATE_CMD --input-dir /tmp/generate/templates/kubernetes --output-dir /tmp/clusters/test -d "env=/tmp/generate/values/test.yaml"
+  $GOMPLATE_CMD --input-dir /tmp/generate/templates/kubernetes --output-dir /tmp/clusters/prod -d "env=/tmp/generate/values/prod.yaml"
+
+  echo "Generating docker-compose file with gomplate"
+  $GOMPLATE_CMD --input-dir /tmp/generate/templates/docker-compose --output-dir /tmp/clusters/docker-compose -d "env=/tmp/generate/values/e2e.yaml"
 }
 
 
