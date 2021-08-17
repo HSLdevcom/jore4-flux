33632a7fcfb6973585f671cace5b366d64c289b9
Tamas Pallos <tamas.pallos@vincit.fi>
2021-08-17 14:57:22 +0300

    Generate full Kubernetes manifest for each stage with gomplate

--- development.sh
+++ development.sh
@@ -2,14 +2,32 @@
 
 set -eu
 
-function generate_kustomize_patches {
-  echo "Generating Kustomize patches with gomplate"
+function generate_manifests {
+  echo "Generating Kubernetes manifests with gomplate"
 
-  GOMPLATE_CMD="docker run --rm -v $(pwd):/tmp hairyhenderson/gomplate@sha256:8e46d887a73ef5d90fde1f1a7d679fa94cf9f6dfc686b0b1a581858faffb1e16"
-  $GOMPLATE_CMD --input-dir /tmp/generate/templates/kustomize-patches --output-dir /tmp/clusters/playg --context Values=/tmp/generate/values/playg.yaml
-  $GOMPLATE_CMD --input-dir /tmp/generate/templates/kustomize-patches --output-dir /tmp/clusters/dev --context Values=/tmp/generate/values/dev.yaml
-  $GOMPLATE_CMD --input-dir /tmp/generate/templates/kustomize-patches --output-dir /tmp/clusters/test --context Values=/tmp/generate/values/test.yaml
-  $GOMPLATE_CMD --input-dir /tmp/generate/templates/kustomize-patches --output-dir /tmp/clusters/prod --context Values=/tmp/generate/values/prod.yaml
+  GOMPLATE_CMD="docker run --rm -v $(pwd):/tmp hairyhenderson/gomplate@sha256:8e46d887a73ef5d90fde1f1a7d679fa94cf9f6dfc686b0b1a581858faffb1e16 \
+    --template templates=/tmp/generate/templates/resources/ \
+    -d common=/tmp/generate/values/common.yaml"
+  TEMPLATES_DIR="/tmp/generate/templates"
+  OUTPUT_DIR="/tmp/clusters"
+  STAGES=("playg" "dev" "test" "prod" "e2e")
+
+  # generate manifests for each stage
+  for STAGE in "${STAGES[@]}"
+  do
+    $GOMPLATE_CMD \
+    --input-dir "$TEMPLATES_DIR/kubernetes-all" \
+    --output-dir "/tmp/clusters/$STAGE" \
+    -d "env=/tmp/generate/values/$STAGE.yaml" \
+    -c "Values=merge:env|common"
+  done
+
+  # generate additions to e2e stage (e.g. test databases)
+  $GOMPLATE_CMD \
+    --input-dir "$TEMPLATES_DIR/kubernetes-e2e-only" \
+    --output-dir "$OUTPUT_DIR/e2e" \
+    -d "env=/tmp/generate/values/e2e.yaml" \
+    -c "Values=merge:env|common"
 }
 
 function super_linter {
@@ -29,7 +47,7 @@ function usage {
   Usage $0 <command>
 
   generate
-    Generates Kustomize patches for playg, dev, test and prod stages using gomplate yaml templates.
+    Generates Kubernetes and docker-compose manifests for all stages using gomplate yaml templates.
 
   lint
     Runs Github's Super-Linter for the whole codebase to lint all files.
@@ -44,7 +62,7 @@ function usage {
 
 case $1 in
 generate)
-  generate_kustomize_patches
+  generate_manifests
   ;;
 
 lint)
